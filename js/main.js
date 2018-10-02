ac623507aa394229ecf77da3081c3c57796054cd
Ethan Casady <ethancasady1@gmail.com>
2018-10-01 21:04:13 -0600

    Initial upload of repository.

---
+++ js/main.js
@@ -0,0 +1,60 @@
+
+
+//codepen console write
+/*
+var c = function() {
+    return({
+        log: function(msg) {
+          consoleDiv = document.getElementById('console');
+          para = document.createElement('p');
+          text = document.createTextNode(msg);
+          para.appendChild(text);
+          consoleDiv.appendChild(para);
+        }
+    });
+}();
+
+c.log("poop");
+*/
+/*
+create a program that will ask the users name, age, and reddit username. have it tell them the information back, in the format:
+
+your name is (blank), you are (blank) years old, and your username is (blank)
+for extra credit, have the program log this information in a file to be accessed later.
+*/
+
+/*
+alert1 = prompt("What is your name?");
+alert2 = prompt("How old are you?");
+alert3 = prompt("What is your username?");
+function userInfo(name, age, username) {
+    var alert1 = name;
+    var alert2 = age;
+    var alert3 = username;
+  c.log('Your name is ' + name + ', you are ' + age + ' years old, and your username is ' + username);
+}
+
+userInfo();
+*/
+//userInfo('John', 27, 'bobface');
+
+//var userJohn = userInfo('John', 25, 'bobface');
+//c.log(userJohn);
+
+//FINAL ANSWER
+/*
+function userInfo(name, age, username) {
+  c.log('Your name is ' + name + ', you are ' + age + ' years old, and your username is ' + username + '.');
+}
+
+userInfo(prompt("What is your name?"), prompt("How old are you?"), prompt("What is your username?"));
+*/
+//CHALLENGE 2
+
+/*
+
+Hello, coders! An important part of programming is being able to apply your programs, so your challenge for today is to create a calculator application that has use in your life. It might be an interest calculator, or it might be something that you can use in the classroom. For example, if you were in physics class, you might want to make a F = M * A calc.
+
+EXTRA CREDIT: make the calculator have multiple functions! Not only should it be able to calculate F = M * A, but also A = F/M, and M = F/A!
+
+*/
